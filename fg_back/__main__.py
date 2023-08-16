368d79940445b72f1b7ca4daecc7aaf588a4f16a
sentenzo <sentenzo@users.noreply.github.com>
2023-08-16 12:54:08 +0300

    crude experiments

--- fg_back/__main__.py
+++ fg_back/__main__.py
@@ -1,7 +1,32 @@
+import os
+
+import dotenv
 import uvicorn
-from fastapi import FastAPI, HTTPException, status
+from fastapi import FastAPI, HTTPException, Request, status
+from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import ValidationException
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel, Field
 
 app = FastAPI(title="fg")
+
+
+dotenv.load_dotenv()
+
+
+DEBUG = str(os.getenv("DEBUG", False)).lower() in ("true", "1")
+
+
+if DEBUG:
+
+    @app.exception_handler(ValidationException)
+    async def validation_exception_handler(
+        request: Request, exc: ValidationException
+    ):
+        return JSONResponse(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            content=jsonable_encoder({"detail": exc.errors()}),
+        )
 
 
 @app.get("/ping/")
@@ -30,13 +55,44 @@ def get_user_by_id(id: int):
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
 
 
-db_items = [{"id": i, "name": f"item {i}"} for i in range(1, 100)]
+class Tag(BaseModel):
+    id: int
+    name: str
 
 
-@app.get("/api/items/")
+class Item(BaseModel):
+    id: int | None = None
+    user_id: int
+    name: str
+    price_cents: int = Field(ge=0)
+    tags: list[Tag] = Field(min_length=1)
+
+
+db_items = [
+    {
+        "id": i,
+        "name": f"item {i}",
+        "user_id": 1,
+        "price_cents": 1234,
+        "tags": [{"id": 1, "name": "tag 1"}],
+    }
+    for i in range(1, 100)
+]
+
+
+@app.get(
+    "/api/items/",
+    response_model=list[Item],
+)
 def get_all_items(page: int = 1, limit: int = 6):
     # return db_items[(page - 1) * limit : (page) * limit]
     return db_items[(page - 1) * limit :][:limit]
+
+
+@app.post("/api/items/many/")
+def add_items(items: list[Item]):
+    db_items.extend(items)
+    return {"status": 200, "data": db_items}
 
 
 if __name__ == "__main__":
